6be816616b015e4c69ec9f87bc5e5bc1d2fa824e
Michael Matich <matich.mike@gmail.com>
2020-05-12 16:05:37 -0400

    first day commit + daylist and interviewerlist components added

---
+++ src/components/DayListItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
+export default function DayListItem(props) {
+  function formatSpots(props) {
+    return props.spots === 0 ? "no spots remaining"
+         : (props.spots === 1 ? "1 spot remaining"
+         : `${props.spots} spots remaining`)
+  }
+
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)} data-testid="day">
+      <h2>{props.name}</h2>
+      <h3>{formatSpots(props)}</h3>
+    </li>
+  );
+}
