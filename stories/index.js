5c537a439f99adbbe45ae7c35c3bc7e04a08a3dc
Michael Matich <matich.mike@gmail.com>
2020-05-12 20:51:39 -0400

    Show, Confirm, Status, Error functions added

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,10 @@ import InterviewerListItem from "components/InterviewerListItem";
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 storiesOf("Button", module)
   .addParameters({
@@ -138,4 +142,12 @@ storiesOf("Button", module)
         .add("Appointment", () => <Appointment />)
         .add("Appointment with Time", () => <Appointment time="12pm" />)
         .add("Header", () => <Header time="12pm" />)
-        .add("Empty", () => <Empty onAdd={action("onAdd")} />);
+        .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+        .add("Show", () => <Show student="Lydia Miller-Jones" interviewer={interviewer} onEdit={action("onEdit")} onDelete={action("onDelete")}/>)
+        .add("Confirm", () => <Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")} />)
+        .add("Status", () => <Status message="Deleting" />)
+        .add("Error", () => <Error message="Could not delete appointment." onClose={action("onClose")} />)
+
+
+
+        
