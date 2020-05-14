1834941d835619649c975deae6c4ca4021035c58
Michael Matich <matich.mike@gmail.com>
2020-05-13 21:49:43 -0400

    combined state added for day/days/appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,6 +3,7 @@ import Appointment from 'components/Appointment';
 import "components/Application.scss";
 import DayList from "components/DayList";
 import axios from "axios";
+import { getAppointmentsForDay } from "helpers/selectors";
 
 const appointments = [
   {
@@ -51,14 +52,20 @@ const appointments = [
 
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
+  
   useEffect(() => {
     axios({
       method: "GET",
       url: `/api/days`})
       .then((response) => {
-        setDays(response.data)
+        setDays(response.data);
     })
   }, []);
   const schedule = appointments.map((appointment) => {
@@ -78,7 +85,7 @@ export default function Application(props) {
 <hr className="sidebar__separator sidebar--centered" />
 <nav className="sidebar__menu">
 
-  <DayList days={days} day={day} setDay={setDay} />
+  <DayList days={state.days} day={state.day} setDay={setDay} />
 
 </nav>
 <img
