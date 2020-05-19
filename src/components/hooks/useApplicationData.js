19da4a562b827b6dcfdf3f807605042820c7d64b
Michael Matich <matich.mike@gmail.com>
2020-05-19 13:30:36 -0400

    axios mock test added

--- src/components/hooks/useApplicationData.js
+++ src/components/hooks/useApplicationData.js
@@ -79,15 +79,9 @@ export default function useApplicationData() {
   
   useEffect(() => {
     Promise.all([
-      axios({
-        method: "GET",
-        url: `/api/days`}),
-      axios({
-        method: "GET",
-        url: `/api/appointments`}),
-        axios({
-          method: "GET",
-          url: `/api/interviewers`})
+      axios.get(`/api/days`),
+      axios.get(`/api/appointments`),
+      axios.get(`/api/interviewers`)
     ])
       .then((all) => {
         setState({ days: all[0].data, appointments: all[1].data, interviewers: all[2].data });
